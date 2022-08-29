f752a99e783cdc25c13869d274958e8c467407f7
Zero-st <2505560951@qq.com>
2022-08-29 20:57:59 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ul>
+      <li>
+        <router-link to="/Login">Login</router-link>
+      </li>
+    </ul>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
